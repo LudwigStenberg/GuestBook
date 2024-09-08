486aa70c57359620048a0c4cc57530c62d630f17
LudwigStenberg <contact@ludwigstenberg.com>
2024-09-08 08:54:45 +0200

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using GuestBookLibrary;
+
+namespace GuestBook;
+
+class Program
+{
+    private static List<GuestModel> guests = new List<GuestModel>();
+
+    static void Main(string[] args)
+    {
+        Console.WriteLine("=== Guest Book ===");
+
+        GetGuestInformation();
+        PrintGuestInformation();
+
+        Console.ReadLine();
+    }
+
+    private static void GetGuestInformation()
+    {
+        string moreGuestsComing = "";
+
+        do // Run it at least once
+        {
+            GuestModel guest = new GuestModel(); // This represents ONE guest!
+            
+            guest.FirstName = GetInfoFromConsole("What is your first name: ");
+            guest.LastName = GetInfoFromConsole("What is your last name: ");
+            guest.MessageToHost = GetInfoFromConsole("What message would you like to tell your host: ");
+            moreGuestsComing = GetInfoFromConsole("Are more guests coming? (yes/no)");
+            
+            guests.Add(guest);
+
+            Console.Clear();
+
+        } while (moreGuestsComing.ToLower() == "yes"); // As long as..
+    }
+
+    private static void PrintGuestInformation()
+    {
+        foreach (GuestModel guest in guests)
+        {
+            Console.WriteLine(guest.GuestInfo);
+        }
+    }
+
+    private static string GetInfoFromConsole(string message) // Refactors 
+    {
+        string output = "";
+
+        Console.Write(message);
+        output = Console.ReadLine();
+
+        return output;
+    }
+}
